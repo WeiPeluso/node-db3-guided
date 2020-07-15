ceed4de2760142d9a71758115757cb9451904ff2
Wei Peluso <weipeluso@MacBook-Pro.local>
2020-07-15 14:14:54 -0400

    finished

---
+++ users/user-model.js
@@ -0,0 +1,33 @@
+const db = require("../data/db-config.js");
+
+module.exports = {
+  getAll,
+  findById,
+  update,
+  add,
+};
+
+function getAll() {
+  return db("users");
+}
+
+function findById(id) {
+  return db("users").where({ id }).first();
+}
+
+function update(id, changes) {
+  return db("users")
+    .where({ id })
+    .update(changes)
+    .then(() => {
+      return findById(id);
+    });
+}
+
+function add(user) {
+  return db("users")
+    .insert(user, "id")
+    .then(([id]) => {
+      return findById(id);
+    });
+}
